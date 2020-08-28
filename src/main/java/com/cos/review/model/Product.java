e81b4a0776f7d467a8fb37c3dfceb5d8119ade13
mary <qldhffk112@gmail.com>
2020-08-28 12:26:46 +0900

    날짜 파싱 테스트

---
+++ src/main/java/com/cos/review/model/Product.java
@@ -0,0 +1,36 @@
+package com.cos.review.model;
+
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Product {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String blogUrl;
+	private String title;
+	@Column(length= 1000)
+	private String thumnail;
+	private String day;
+	
+	@CreationTimestamp
+	private Timestamp createDate;
+}
